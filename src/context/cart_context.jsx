1b78cde9ea90c1e9c39df34a89d1b47eec277202
VinhQua <dragonlinuxnoinochi@gmai.com>
2023-05-31 21:32:53 +0900

    add cart item

--- src/context/cart_context.jsx
+++ src/context/cart_context.jsx
@@ -9,7 +9,7 @@ import {
 } from "../actions";
 
 const initialState = {
-  cart: [],
+  cart: JSON.parse(localStorage.getItem("cart")) || [],
   total_items: 0,
   total_amount: 0,
   shipping_fee: 534,
@@ -24,12 +24,22 @@ export const CartProvider = ({ children }) => {
     dispatch({ type: ADD_TO_CART, payload: { id, color, amount, product } });
   };
   //remove item
-  const removeItem = (id) => {};
+  const removeItem = (id) => {
+    dispatch({ type: REMOVE_CART_ITEM, payload: id });
+  };
   //toggle amount
-  const toggleAmount = (id, value) => {};
+  const toggleAmount = (id, value) => {
+    console.log(id, value);
+    dispatch({ type: TOGGLE_CART_ITEM_AMOUNT, payload: { id, value } });
+  };
   //clear cart
-  const clearCart = () => {};
+  const clearCart = () => {
+    dispatch({ type: CLEAR_CART });
+  };
 
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(state.cart));
+  }, [state.cart]);
   return (
     <CartContext.Provider
       value={{ ...state, addToCart, removeItem, toggleAmount, clearCart }}
--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -38,6 +38,21 @@ const cart_reducer = (state, action) => {
       return { ...state, cart: [...state.cart, newItem] };
     }
   }
+  if (action.type === REMOVE_CART_ITEM) {
+    const tempCart = state.cart.filter((item) => item.id !== action.payload);
+    return { ...state, cart: tempCart };
+  }
+  if (action.type === CLEAR_CART) {
+    return { ...state, cart: [] };
+  }
+  if (action.type === TOGGLE_CART_ITEM_AMOUNT) {
+    const { id, value } = action.payload;
+    const tempCart = state.cart.map((item) => {
+      return item;
+    });
+
+    return { ...state, cart: tempCart };
+  }
   // return state;
   throw new Error(`No Matching "${action.type}" - action type`);
 };
