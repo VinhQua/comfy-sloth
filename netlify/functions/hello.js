fb8571b117053ca9ed269dc2bcf72c9242e5a6a2
VinhQua <dragonlinuxnoinochi@gmai.com>
2023-06-02 09:40:17 +0900

    add functions

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -3,6 +3,6 @@
 exports.handler = async function (event, context) {
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "hello world" }),
+    body: JSON.stringify({ message: "Hello World" }),
   };
 };
